3eafc5c7406d9a1d2c3379d286b6d3ad3ff03354
qian-nan-xu <xuqiannan@huawei.com>
2022-03-18 15:03:42 +0800

    CODEX
    Signed-off-by: qian-nan-xu <xuqiannan@huawei.com>

--- dataBusiness/contacts/src/contacts_type.cpp
+++ dataBusiness/contacts/src/contacts_type.cpp
@@ -70,8 +70,8 @@ void ContactsType::PrepopulateCommonTypes(std::shared_ptr<OHOS::NativeRdb::RdbSt
     vectorType.push_back(ContentTypeData::HICALL_DEVICE);
     vectorType.push_back(ContentTypeData::CAMCARD);
     vectorType.push_back(ContentTypeData::SIP_ADDRESS);
-    unsigned int size = vectorType.size();
-    for (int i = 0; i < (int)size; i++) {
+    int size = (int)vectorType.size();
+    for (int i = 0; i < size; i++) {
         std::string typeValue = vectorType[i];
         int error = LookupTypeId(rdbStore, typeValue);
         if (error == RDB_EXECUTE_FAIL) {
